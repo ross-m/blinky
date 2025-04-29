102e2356f2eab9e8dc2b4921ec3e8e4845bbdc6f
Ross Morrison <rossmorrison53@yahoo.com>
2025-04-28 21:41:22 -0500

    First commit!

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <stdbool.h>
+#include <stdint.h>
+#include "inc/hw_memmap.h"
+
+#define RCGCGPIO_R *(volatile uint32_t*) 0x400FE608 // SYSCTL clock register for port F
+#define GPIODIR_F_R *(volatile uint32_t*) 0x4005D400
+
+void initLED(void)
+{
+    RCGCGPIO_R |= (1 << 5); // Enable the clock on port F
+    volatile uint32_t dummy;
+    dummy = RCGCGPIO_R; // Dummy read to introduce 3-cycle delay (required before accessing the port we just enabled)
+    GPIODIR_F_R |= (1 << 1); // Set LED pin to output mode
+}
+
+int main(void) {
+    initLED();
+}
+
